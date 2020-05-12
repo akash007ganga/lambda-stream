d12d3d7046d48cbb36fc3d647ec3648a0938925e
pal chakraborty <akash007ganga@gmail.com>
2020-05-12 22:23:00 +0530

    Eclipse git integration for lambda-stream practice

---
+++ lambda.stream/src/main/java/com/palc/lambda/GitTest.java
@@ -0,0 +1,16 @@
+package com.palc.lambda;
+
+/**
+ * @author echypal
+ *
+ */
+public class GitTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Git is fun");
+	}
+
+}
